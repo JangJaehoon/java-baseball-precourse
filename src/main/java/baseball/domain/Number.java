9f6710314420c18172c44317594e052007cf989c
JangJaehoon <jjhgwx@gmail.com>
2022-04-18 00:30:51 +0900

    수정

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -1,21 +1,18 @@
 package baseball.domain;
 
-
 import baseball.exception.RangeException;
 import java.util.Objects;
 
 public class Number {
-    private static final Integer MINIMUM = 1;
-    private static final Integer MAXIMUM = 9;
+    private static final Integer MIN = 1;
+    private static final Integer MAX = 9;
     private final int Numbers;
 
     public Number(Character number) {
-
         this(Character.getNumericValue(number));
     }
 
     public Number(String number) {
-
         this(Integer.parseInt(number));
     }
 
@@ -25,21 +22,19 @@ public class Number {
     }
 
     private void validateNum(Integer number) {
-        if (MINIMUM > number || MAXIMUM < number) {
+        if (MIN > number || MAX < number) {
             throw new RangeException();
         }
     }
 
-
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
+        if (this == o)
             return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
+
+        if (o == null || getClass() != o.getClass())
             return false;
-        }
+
         Number that = (Number) o;
         return Numbers == that.Numbers;
     }
